4362d77c98461aefa096df195607c6e0c436ae93
ghanshujee4 <ghanshujee4@gmail.com>
2017-01-06 18:05:34 +0530

    Com
    com2

---
+++ MI News/js/main.js
@@ -0,0 +1,54 @@
+﻿'use strict';
+var app = angular.module('App', []);
+app.controller('MyController', function ($scope) {
+    debugger
+    $scope.name = "Ghnahsyam jha";
+    $scope.gallery = "Our Gallery";
+    //debugger;    //$scope.img1 = "images/g2.jpg";
+    //$scope.name = 'abc';
+    //debugger;
+    
+    $scope.slides = [
+        {
+            image: 'g2.jpg'
+        },
+        {
+            image: 'g4.jpg'
+
+        },
+        {
+        image: 'g5.jpg'
+    },
+    {
+    image: 'g6.jpg'
+    }, {
+        image: 'g1.jpg'
+    },
+    {
+        image: 'g3.jpg'
+    }
+    ];
+});
+
+
+
+//app.config(['$routeProvider',
+//function($routeProvider) {
+//    debugger;
+//    $routeProvider
+//    .when("/", {
+//        templateUrl: "index.html",
+//        controller: 'AddOrderController'
+//    })
+//    .when("/about", {
+//        templateUrl: "About.html"
+//    })
+//    .when("/services", {
+//        templateUrl: "Services.html"
+//    })
+//    .when("/blue", {
+//        templateUrl: "Gallery.htm"
+//    });
+//}]);
+
+
